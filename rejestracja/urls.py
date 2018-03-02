d825856a4dfa2fab4577fa6df7c7229597ff8247
Jarek <spsp01@o2.pl>
2018-03-02 14:46:31 +0100

    mode

--- rejestracja/urls.py
+++ rejestracja/urls.py
@@ -5,5 +5,6 @@ app_name = 'rejestracja'
 urlpatterns = [
     path('', views.RejestracjaView.as_view(), name='index' ),
     path('jan-kowalski', views.DietetykView.as_view(), name='jan-kowalki'),
-    path('ada-wspaniala', views.DietetykView.as_view(), name='ada-wspaniala')
+    path('ada-wspaniala', views.DietetykView.as_view(), name='ada-wspaniala'),
+    path('test', views.detail, name='test' )
 ]
--- rejestracja/views.py
+++ rejestracja/views.py
@@ -4,6 +4,8 @@ from .forms import RejestracjaForm
 from django.views.generic import TemplateView, DetailView, CreateView
 from .test import dni
 from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 class RejestracjaView(TemplateView):
     template_name = 'rejestracja/dietetycy.html'
@@ -28,14 +30,53 @@ class DietetykView(CreateView):
         context['niedostepne'] = gm
         return context
 
-    # def post(self, request, *args, **kwargs):
-    #     form =RejestracjaForm(request.POST)
-    #     context_new = {
-    #         'object': 'OK',
-    #        }
-    #     if form.is_valid():
-    #        print('ok')
-    #
-    #     return render(request,'rejestracja/rejestracja.html',context_new)
+    def post(self, request, *args, **kwargs):
+         form = RejestracjaForm(request.POST)
+
+         context_new = {
+             'object': 'OK',
+            }
+         if form.is_valid():
+            imie = form.cleaned_data['imię']
+            email = form.cleaned_data['email']
+            dietetyk = form.cleaned_data['dietetyk']
+            dzień = form.cleaned_data['dzień']
+            godzina = form.cleaned_data['godzina']
+            obj, created = Rejestracja.objects.get_or_create(imię=imie,email=email,dietetyk=dietetyk,dzień=dzień,godzina=godzina)
+            if created:
+                obj.save()
+                return JsonResponse(context_new)
+            else:
+                context_new = {
+                    'object': 'Nie można zarejestrować - podana data i godzina jest już zarezerwowana',
+                }
+         return JsonResponse(context_new)
 
 
+def detail(request, *args, **kwargs):
+    if request.method == "POST":
+        imie = request.POST.get('imię')
+        email = request.POST.get('email')
+        dietetyk = request.POST.get('dietetyk')
+        dzień = request.POST.get('dzień')
+        godzina = request.POST.get('godzina')
+
+        print(imie)
+        print(email)
+        print(dietetyk)
+        print(dzień)
+        print(godzina)
+
+        #Prosta Walidacja długości imienia
+        if len(imie) > 3:
+            print(imie)
+            Rejestracja(imię=imie,email=email,dietetyk=dietetyk,dzień=dzień,godzina=godzina)
+            Rejestracja.save(self)
+
+
+        #Walidacja maila
+        email = request.POST.get('email')
+        #test = Rejestracja.objects.all()
+
+        return HttpResponse({'imię':imie,'email':email})
+    return render(request, 'rejestracja/rejestracja_formc.html', {'poll': 'p'})
